b514fd89a1749f91b54d957b2adacd934b76b767
nmartens40-rrc <Nmartens40@rrc.ca>
2026-01-15 11:10:52 -0600

    Fixed CSS styling and layout, and fixed issues found by Axe Dev Tools.

--- src/components/common/Left-Side-Bar/leftSIdeBar.tsx
+++ src/components/common/Left-Side-Bar/leftSIdeBar.tsx
@@ -3,11 +3,14 @@ function LeftSideBar(props: {movies:string[]}) {
     return( 
     <div id="topFiveMovies">
         <h2>Top 5 Movies</h2>
-        <ol>1. {props.movies[0]}</ol>
-        <ol>2. {props.movies[1]}</ol>
-        <ol>3. {props.movies[2]}</ol>
-        <ol>4. {props.movies[3]}</ol>
-        <ol>5. {props.movies[4]}</ol>
+        <ol>
+            <li>{props.movies[0]}</li>
+            <li>{props.movies[1]}</li>
+            <li>{props.movies[2]}</li>
+            <li>{props.movies[3]}</li>
+            <li>{props.movies[4]}</li>
+        </ol>
+        
     </div>
     )
 }
