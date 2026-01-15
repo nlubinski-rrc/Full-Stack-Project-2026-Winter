cb3b418c6fcd0ff0be203d275ddf4527fc62b785
Nolan Lubinski <nlubinski@academic.rrc.ca>
2026-01-15 10:01:34 -0600

    Implemented the left side bar for use and made a json movies.tsx that just creates a list of movies and returns it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Footer from './components/common/footer/Footer.tsx'
 import Nav from './components/common/nav/Nav.tsx';
+import LeftSideBar from './components/common/Left-Side-Bar/leftSIdeBar.tsx'
+import movies from './components/common/jsonMovies.tsx'
 
 function App() {
   return (
     <>
       <Nav />
       <Footer /> 
+      <LeftSideBar movies={movies} />
     </>
   );
 };
