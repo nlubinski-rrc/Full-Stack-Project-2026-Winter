0b168b59fc30ab1e23a5fa42ba3efd4bb720522b
Dylan Kaspick <dkaspick2@academic.rrc.ca>
2026-01-25 15:28:17 -0600

    Creating all compenents needed to implement feature.

---
+++ src/components/pages/FavoriteActors.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+type Actor = {
+    id: number
+    name: string,
+    isFavorite: boolean
+}
+
+function FavoriteActorPage() {
+    const [actors, setActors] = useState<Actor[]>([
+        {id: 1, name: "Ryan Gosling", isFavorite: false},
+        {id: 2, name: "Harrison Ford", isFavorite: false},
+        {id: 3, name: "Ana de Armas", isFavorite: false},
+        {id: 4, name: "Sylvia Hoeks", isFavorite: false},
+        {id: 5, name: "Robin Wright", isFavorite: false}
+    ]);
+
+    return(<></>)
+}
+
+export default FavoriteActorPage;
