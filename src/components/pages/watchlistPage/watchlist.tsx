654fd761e0bdf5514e4f90318701df6361452b33
nmartens40-rrc <Nmartens40@rrc.ca>
2026-01-26 10:37:35 -0600

    Added watchlistpage files and adjusted topReviewers text colouring.

---
+++ src/components/pages/watchlistPage/watchlist.tsx
@@ -0,0 +1,11 @@
+import "./watchlist.css"
+
+function WatchlistPage() {
+    return (
+        <div id="watchlist-page">
+            <h1 id="title-header">My Watchlist</h1>
+        </div>
+    )
+}
+
+export default WatchlistPage
