f04b5899b681014471048f885dee44d13dc641f4
Nolan Lubinski <nlubinski@academic.rrc.ca>
2026-01-29 11:29:44 -0600

    Made a page for my implementation createReviews

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,38 @@ import './App.css'
 import Footer from './components/common/footer/footer.tsx'
 import Nav from './components/common/nav/Nav.tsx';
 import MovieGrid from './components/common/movie-grid/movieGrid.tsx';
-import { CreateReview } from './components/common/createReview/createReview.tsx';
 import LeftSideBar from './components/common/Left-Side-Bar/leftSIdeBar.tsx'
 import movies from './components/common/jsonMovies.tsx'
 import TopReviewersSideBar from './components/common/top-reviewers-side-bar/topReviewers.tsx';
-
+import {Routes, Route} from "react-router-dom"
+import { CreateReview } from './components/common/createReview/createReview.tsx';
 function App() {
   return (
     <>
-      <Nav />
+    <Nav></Nav>
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <>
       <section id='main-section'>
         <LeftSideBar movies={movies} />
         <MovieGrid />
         <TopReviewersSideBar />
       </section>
-      <CreateReview />
-      <Footer /> 
+      <Footer />
+      </>
+        }/>
+      <Route
+      path='//create-reviews'
+      element={
+        <>
+        <CreateReview/>
+        </>
+      }
+      />
+    </Routes>
     </>
   );
 };
-
 export default App
--- src/components/common/createReview/createReview.tsx
+++ src/components/common/createReview/createReview.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react"
-import "./createReview.css"
+import { useState } from "react";
+import "./createReview.css";
 export function CreateReview() {
-    const [text, textSave] = useState("")
-    const [error, setError] = useState("")
+    const [text, textSave] = useState("");
+    const [error, setError] = useState("");
     const [reviews, setReviews] = useState<string[]>([]);
-    const [deleteText, setDeleteText] = useState("")
+    const [deleteText, setDeleteText] = useState("");
 
     function handleChange(e) {
         textSave(e.target.value)
-        if (error) setError("")
+        if (error) {
+            setError("")
+        }
     }
     function handleChangeDeleteText(e) {
         setDeleteText(e.target.value)
@@ -23,12 +25,10 @@ export function CreateReview() {
     function removeReview(index: number) {
         setReviews( prevItems => { 
             Number(index)
-            console.log(index)
             if (index <= -1 || index >= prevItems.length) {
             setError("Review you're trying to delete doesn't exist");
             return prevItems; // IMPORTANT
             }
-
             const newItems = [...prevItems];
             newItems.splice(index, 1);
             setError("");
@@ -36,17 +36,17 @@ export function CreateReview() {
             });
     }
     return(
-        <div id="form">
-            <h3>Please be sure to include your movie title</h3>
+        <div id="reviewform">
+            <h2>leave a review and please be sure to include your movie title</h2>
             <textarea value={text} onChange={handleChange} style={{width: '300px', height: '50px'}}>Review message</textarea>
             <button onClick={saveText}>
-                Submit
+                Submit review
             </button>
             <div id="reviewStage">
                 <h2>Recent reviews:</h2>
-                <input id="numberInput"type="number" value={deleteText} onChange={handleChangeDeleteText} placeholder="Select which review to delete"/>
+                <input id="numberInput" type="number" value={deleteText} onChange={handleChangeDeleteText} placeholder="Select which review to delete"/>
                 <button onClick={() => removeReview(Number(deleteText) - 1)}>
-                    Submit
+                    Remove Review
                 </button>
                 <ol>
                     {reviews.map((review, index) => (
