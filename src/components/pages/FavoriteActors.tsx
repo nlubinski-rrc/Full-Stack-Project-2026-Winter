0ac3cecc0069e773adfe5b7ce3c6e48892d37792
Dylan Kaspick <dkaspick2@academic.rrc.ca>
2026-01-30 14:36:15 -0600

    ActorCard component complete.

--- src/components/pages/FavoriteActors.tsx
+++ src/components/pages/FavoriteActors.tsx
@@ -1,19 +1,7 @@
 import { useState } from "react";
 
-type Actor = {
-    id: number
-    name: string,
-    isFavorite: boolean
-}
 
 function FavoriteActorsPage() {
-    const [actors, setActors] = useState<Actor[]>([
-        {id: 1, name: "Ryan Gosling", isFavorite: false},
-        {id: 2, name: "Harrison Ford", isFavorite: false},
-        {id: 3, name: "Ana de Armas", isFavorite: false},
-        {id: 4, name: "Sylvia Hoeks", isFavorite: false},
-        {id: 5, name: "Robin Wright", isFavorite: false}
-    ]);
 
     return(<></>)
 }
