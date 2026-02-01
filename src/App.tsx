dbcece580ee4e1a96eec58d751d95404e62456e6
bhavdeepcheema <bhavdeep@academic.rrc.ca>
2026-02-01 13:51:27 -0600

    adding the route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import Footer from './components/common/footer/footer.tsx'
 import Nav from './components/common/nav/Nav.tsx';
 import MovieGrid from './components/common/movie-grid/movieGrid.tsx';
-
+import WatchedMovies from './components/common/watched-movies/watchedmovies.tsx';
 import LeftSideBar from './components/common/Left-Side-Bar/leftSIdeBar.tsx'
 import movies from './components/common/jsonMovies.ts'
 import TopReviewersSideBar from './components/common/top-reviewers-side-bar/topReviewers.tsx';
@@ -38,9 +38,24 @@ function App() {
             </>
           }
         />
+        <Route
+          path="/watched-movies"
+          element={
+            <>
+              <section id="main-section">
+                <LeftSideBar movies={movies} />
+                <WatchedMovies />
+                <TopReviewersSideBar />
+              </section>
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+    </>
+  );
       </Routes>
     </>
   );
 }
-
 export default App;
