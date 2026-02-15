26a8eade35ba2d82da44a53d5d906d49687670ae
nmartens40-rrc <Nmartens40@rrc.ca>
2026-02-15 13:22:26 -0600

    Removed unnecessary import in movie service.

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,4 @@
 import * as MovieRepo from "../apis/movieRepo"
-import type { Movie } from "../types/movie"
 
 export async function fetchMovies() {
     const movies = await MovieRepo.fetchMovies()
