1b9b1fc22d08ddb36ce107f90d0699512e300552
bhavdeepcheema <bhavdeep@academic.rrc.ca>
2026-02-25 19:38:48 -0600

    added the watchlist.ts

---
+++ src/data/watchlist.ts
@@ -0,0 +1,14 @@
+import type { WatchlistItem } from '../assets/types/watchlistType';
+
+export const testWatchlist: WatchlistItem[] = [
+  { id: 1, movieId: "tt1375666", title: "Inception", addedDate: "2026-01-01" },
+  { id: 2, movieId: "tt0133093", title: "The Matrix", addedDate: "2026-01-02" },
+  { id: 3, movieId: "tt0109830", title: "Forrest Gump", addedDate: "2026-01-03" },
+  { id: 4, movieId: "tt0068646", title: "The Godfather", addedDate: "2026-01-04" },
+  { id: 5, movieId: "tt0468569", title: "The Dark Knight", addedDate: "2026-01-05" },
+  { id: 6, movieId: "tt0111161", title: "Shawshank Redemption", addedDate: "2026-01-06" },
+  { id: 7, movieId: "tt0120737", title: "Gladiator", addedDate: "2026-01-07" },
+  { id: 8, movieId: "tt0167260", title: "Lord of the Rings", addedDate: "2026-01-08" },
+  { id: 9, movieId: "tt0137523", title: "Fight Club", addedDate: "2026-01-09" },
+  { id: 10, movieId: "tt0099685", title: "Goodfellas", addedDate: "2026-01-10" },
+];
