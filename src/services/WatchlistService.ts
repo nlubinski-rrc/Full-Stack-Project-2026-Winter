3fb13e916e8f223d8929c992bca24c3b06a9b4cb
bhavdeepcheema <bhavdeep@academic.rrc.ca>
2026-02-25 20:16:10 -0600

    added the watchlistService.ts

---
+++ src/services/WatchlistService.ts
@@ -0,0 +1,15 @@
+import { WatchlistRepository } from '../repositories/WatchlistRepository';
+
+type WatchlistItem = {
+  movieId: number;
+  movieTitle: string;
+};
+
+export class WatchlistService {
+  constructor(private repo = new WatchlistRepository()) {}
+
+  async searchWatchlist(query: string): Promise<WatchlistItem[]> {
+    if (!query.trim()) return [];
+    return this.repo.search(query);
+  }
+}
