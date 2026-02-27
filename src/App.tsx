f8846cb6434f11f87179a99c5e327c484f82f543
Dylan Kaspick <dkaspick2@academic.rrc.ca>
2026-02-27 14:49:41 -0600

    Clean up commented code for PR

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,13 @@ import TopReviewersSideBar from "./components/common/top-reviewers-side-bar/topR
 import WatchlistPage from "./components/pages/watchlistPage/watchlist.tsx";
 import FavoriteActorsPage from "./components/pages/favorite-actors/FavoriteActors.tsx";
 import WatchedMovies from "./components/common/watched-movies/watchedmovies.tsx";
-// import { actorData } from "./components/pages/favorite-actors/actorData.ts";
 import { Routes, Route } from "react-router-dom";
 import { CreateReview } from "./components/common/createReview/createReview.tsx";
 import { useState } from "react";
 import type { Watchlist } from "./assets/types/watchlistType.ts";
-// import type { Actor } from "./types/actor.ts";
 
 function App() {
     const [userWatchlist, setWatchlist] = useState<Watchlist>({ watchlistItems: [] });
-    // const [actors, updateActors] = useState<Actor[]>(actorData);
     return (
         <>
             <Nav></Nav>
@@ -55,8 +52,6 @@ function App() {
                     path="/favorite-actors"
                     element={
                         <FavoriteActorsPage
-                            // actors={actors}
-                            // updateActors={updateActors}
                             userWatchlist={userWatchlist}
                             setWatchlist={setWatchlist}
                         />
