4fc67f4142f0ed47c341ec14be926f6a6cb1939a
Dylan Kaspick <dkaspick2@academic.rrc.ca>
2026-04-23 13:20:05 -0500

    Updated actorRepo to use auth

--- frontend/src/apis/actorRepo.ts
+++ frontend/src/apis/actorRepo.ts
@@ -22,8 +22,14 @@ function mapBackendActor(actor: BackendActor): Actor {
     };
 }
 
-export async function fetchActors(): Promise<Actor[]> {
-    const res = await fetch(`${BASE_URL}${ACTORS_ENDPOINT}`);
+export async function fetchActors(sessionToken?: string|null): Promise<Actor[]> {
+    const res = await fetch(`${BASE_URL}${ACTORS_ENDPOINT}`,
+        sessionToken? {
+            headers: {
+                Authorization: `Bearer ${sessionToken}`
+            }
+        }: undefined
+    );
 
     if (!res.ok) {
         throw new Error("Failed to fetch actors");
@@ -33,8 +39,14 @@ export async function fetchActors(): Promise<Actor[]> {
     return json.data.map(mapBackendActor);
 }
 
-export async function getActorById(actorId: number): Promise<Actor> {
-    const res = await fetch(`${BASE_URL}${ACTORS_ENDPOINT}/${actorId}`);
+export async function getActorById(actorId: number, sessionToken?: string|null): Promise<Actor> {
+    const res = await fetch(`${BASE_URL}${ACTORS_ENDPOINT}/${actorId}`,
+        sessionToken? {
+            headers: {
+                Authorization: `Bearer ${sessionToken}`
+            }
+        }: undefined
+    );
 
     if (!res.ok) {
         throw new Error("Failed to fetch actor");
@@ -44,42 +56,44 @@ export async function getActorById(actorId: number): Promise<Actor> {
     return mapBackendActor(json.data);
 }
 
-export async function addFavouriteActor(actorId: number): Promise<void> {
-    const actor = await getActorById(actorId);
+// FAVOURITES NOW HANDLED BY actorFavouriteRepo
+//
+// export async function addFavouriteActor(actorId: number): Promise<void> {
+//     const actor = await getActorById(actorId);
 
-    const res = await fetch(`${BASE_URL}${ACTORS_ENDPOINT}/${actorId}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            id: actor.id,
-            name: actor.name,
-            isFavourite: true,
-        }),
-    });
+//     const res = await fetch(`${BASE_URL}${ACTORS_ENDPOINT}/${actorId}`, {
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             id: actor.id,
+//             name: actor.name,
+//             isFavourite: true,
+//         }),
+//     });
 
-    if (!res.ok) {
-        throw new Error("Failed to favourite actor");
-    }
-}
+//     if (!res.ok) {
+//         throw new Error("Failed to favourite actor");
+//     }
+// }
 
-export async function deleteFavouriteActor(actorId: number): Promise<void> {
-    const actor = await getActorById(actorId);
+// export async function deleteFavouriteActor(actorId: number): Promise<void> {
+//     const actor = await getActorById(actorId);
 
-    const res = await fetch(`${BASE_URL}${ACTORS_ENDPOINT}/${actorId}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            id: actor.id,
-            name: actor.name,
-            isFavourite: false,
-        }),
-    });
+//     const res = await fetch(`${BASE_URL}${ACTORS_ENDPOINT}/${actorId}`, {
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             id: actor.id,
+//             name: actor.name,
+//             isFavourite: false,
+//         }),
+//     });
 
-    if (!res.ok) {
-        throw new Error("Failed to unfavourite actor");
-    }
-}
+//     if (!res.ok) {
+//         throw new Error("Failed to unfavourite actor");
+//     }
+// }
